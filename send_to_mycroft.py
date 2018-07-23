8dc58044804d7e088e44a9a4740f2b9163c536c5
pcwii <pmchenier@gmail.com>
2018-07-23 19:03:28 -0400

    working on home theatre lighting techniques

--- send_to_mycroft.py
+++ send_to_mycroft.py
@@ -1,3 +1,4 @@
+import sys
 from websocket import create_connection
 import json
 import time
@@ -5,7 +6,12 @@ import time
 URL_TEMPLATE = "{scheme}://{host}:{port}{path}"
 
 
-def send_message(message, host="192.168.0.41", port=8181, path="/core", scheme="ws"):
+socket_host = "192.168.0.41" #this should be the ip address of the mycroft device on the local network
+socket_port = 8181 #this port should match the client port
+socket_msg = sys.argv[1]
+
+
+def send_message(message, host=socket_host, port=socket_port, path="/core", scheme="ws"):
     payload = json.dumps({
         "type": "recognizer_loop:utterance",
         "context": "",
@@ -18,26 +24,29 @@ def send_message(message, host="192.168.0.41", port=8181, path="/core", scheme="
     ws.send(payload)
     ws.close()
 
-# mute Command
-send_message('please be silent')
-print('silent now')
+
+send_message(socket_msg)
 time.sleep(1)
 
-send_message('turn the room lights on')
-time.sleep(0.5)
-send_message('turn the wall lights on')
-time.sleep(10)
-send_message('set the room lights to 10 percent')
-time.sleep(10)
-send_message('turn the room lights off')
-send_message('set the wall lights to orange')
-time.sleep(10)
-send_message('set the wall lights to 2 percent')
-time.sleep(15)
+
+
+
+# mute Command
+# send_message('please be silent')
+# time.sleep(1)
+# send_message('turn the wall lights on')
+# time.sleep(10)
+# send_message('set the room lights to 10 percent')
+# time.sleep(10)
+# send_message('turn the room lights off')
+# send_message('set the wall lights to orange')
+# time.sleep(10)
+# send_message('set the wall lights to 2 percent')
+# time.sleep(15)
 
 # un-mute Command
-send_message('you can speak now')
-print('no longer silent')
-time.sleep(1)
+# send_message('you can speak now')
+# print('no longer silent')
+# time.sleep(1)
 
 
