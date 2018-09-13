effd0661bc6b6d3ce8ca57cb2162b85c5cf3093c
philc <philc@arcx.com>
2018-09-12 20:33:58 -0400

    Rss Feed Reader

--- Cinemavision_Launch.py
+++ Cinemavision_Launch.py
@@ -49,17 +49,51 @@ def list_movies():
         print(e)
         return "NONE"
 
+def show_popup():
+    list_payload ='{"jsonrpc": "2.0", "method": "Addons.ExecuteAddon", ' \
+                  '"params": {"addonid": "script.popup", "params": {"line1": "Hello World", ' \
+                  '"line2": "Showing this message using", "line3": "Combination of Kodi python modules and", ' \
+                  '"line4": "JSON-RPC API interface", "line5": "Have fun coding"}}}'
+    try:
+        list_response = requests.post(kodi_path, data=list_payload, headers=json_header)
+        return list_response.text
+    except Exception as e:
+        # print(e)
+        return e
 
-# kodi_play()
-# cv_play()
-# all_addons = list_addons()
-# if addon_name in all_addons:
-#    print("Found")
-#    cv_play()
 
-#else:
-#    print("Not Found")
-#    kodi_play()
+def clear_playlist():
+    kodi_payload = '{"jsonrpc":"2.0","id":1,"method":"Playlist.Clear","params":{"playlistid":1}}'
+    try:
+        kodi_response = requests.post(kodi_path, data=kodi_payload, headers=json_header)
+        return kodi_response.text
+    except Exception as e:
+        # print(e)
+        return e
 
-#list_movies()
-list_addons()
+def add_playlist():
+    kodi_payload = '{"jsonrpc":"2.0","id":1,"method":"Playlist.Add","params":{"playlistid":1,' \
+                   '"item":{"file":"Media/Big_Buck_Bunny_1080p.mov"}}}'
+    try:
+        kodi_response = requests.post(kodi_path, data=kodi_payload, headers=json_header)
+        return kodi_response.text
+    except Exception as e:
+        # print(e)
+        return e
+
+def mute_kodi():
+    kodi_payload = '{"jsonrpc": "2.0", "id": 1, "method": "Application.SetMute", "params": {"mute":"toggle"}}'
+    try:
+        kodi_response = requests.post(kodi_path, data=kodi_payload, headers=json_header)
+        return kodi_response.text
+    except Exception as e:
+        # print(e)
+        return e
+
+
+# print(list_movies())
+# print(list_addons())
+# print(show_popup())
+# print(clear_playlist())
+# print(add_playlist())
+print(mute_kodi())
